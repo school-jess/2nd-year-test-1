c7c5b41f69c50464796520aa540803a29d82f9ba
invalid-code <jessgaming55@gmail.com>
2025-08-28 12:26:12 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace _2nd_sem_test_1;
+
+class Student
+{
+    
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
